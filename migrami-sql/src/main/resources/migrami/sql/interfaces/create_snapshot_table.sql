4b6083d3b6678dd7bdebe9f2dd624a2d327d02df
Braz <braz@jaya.tech>
2019-10-05 10:04:57 -0300

    Create snapshot table fix

--- migrami-sql/src/main/resources/migrami/sql/interfaces/create_snapshot_table.sql
+++ migrami-sql/src/main/resources/migrami/sql/interfaces/create_snapshot_table.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE migrami_snapshot(
+CREATE TABLE migrami_snapshot(
   category    VARCHAR(100) NOT NULL,
   script_name  VARCHAR(100) NOT NULL,
   checksum    CHAR(32) NOT NULL,
